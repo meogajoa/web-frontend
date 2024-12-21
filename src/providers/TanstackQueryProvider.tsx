637c85a9294d63abb2c754636a2b976e1fea2fa9
jeheecheon <jeheecheon@gmail.com>
2024-12-21 16:17:13 +0900

    chore: add Tanstack query dev tools

--- src/providers/TanstackQueryProvider.tsx
+++ src/providers/TanstackQueryProvider.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function makeQueryClient() {
   return new QueryClient({
@@ -36,7 +37,10 @@ const TanstackQueryProvider: React.FC<React.PropsWithChildren> = ({
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   );
 };
 
