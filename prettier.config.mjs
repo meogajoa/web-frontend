316472e5fd088b0f8ee0875009d24d287d68310f
jeheecheon <jeheecheon@gmail.com>
2024-12-29 01:13:44 +0900

    chore: install and set-up prettier-plugin-organize-imports

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
   tabWidth: 2,
   singleQuote: true,
 };
