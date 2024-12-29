588ced143660993328b9c9c416eda4b93473b8ec
jeheecheon <jeheecheon@gmail.com>
2024-12-29 18:15:06 +0900

    chore: add react-stomp-hooks, install react-hook-form

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { StompSessionProvider } from 'react-stomp-hooks';
 import TanstackQueryProvider from '~/providers/TanstackQueryProvider';
 
 type RootProviderProps = Readonly<React.PropsWithChildren>;
 
 const RootProvider: React.FC<RootProviderProps> = ({ children }) => {
-  return <TanstackQueryProvider>{children}</TanstackQueryProvider>;
+  return (
+    <TanstackQueryProvider>
+      <StompSessionProvider url={'https://stream.elite12.de/api/sock'}>
+        {children}
+      </StompSessionProvider>
+    </TanstackQueryProvider>
+  );
 };
 
 export default RootProvider;
