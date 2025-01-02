07ae6e77c49390f29bf289d9676ec5ef7aff49f8
jeheecheon <jeheecheon@gmail.com>
2025-01-03 01:41:27 +0900

    refactor: improve Button component, add stories

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,16 +2,18 @@ import {
   Button as HeadlessButton,
   ButtonProps as HeadlessButtonProps,
 } from '@headlessui/react';
+import { ChevronDownIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { cva, VariantProps } from 'class-variance-authority';
 import React from 'react';
 import { cn } from '~/utils/classname';
 
 const ButtonVariant = cva(
-  'inline-block w-full text-center font-bold text-white transition duration-300 data-[hover]:scale-95 data-[disabled]:cursor-not-allowed data-[disabled]:bg-gray-5 data-[active]:opacity-70 data-[hover]:opacity-90 data-[focus]:outline-none',
+  'inline-block text-center font-bold text-white transition duration-300 data-[hover]:scale-95 data-[disabled]:cursor-not-allowed data-[disabled]:bg-gray-5 data-[active]:opacity-70 data-[hover]:opacity-90 data-[focus]:outline-none',
   {
     variants: {
       variant: {
         primary: 'bg-red',
+        s차econdary: 'bg-sub',
       },
       rounded: {
         full: 'rounded-full',
@@ -20,6 +22,11 @@ const ButtonVariant = cva(
       size: {
         lg: 'h-12 px-4 py-2.5 text-xl',
         md: 'h-11 w-fit p-2.5 text-base',
+        sm: 'h-9 py-2.5 text-base',
+      },
+      icon: {
+        'chevron-down': 'flex items-center gap-x-1 px-4',
+        plus: 'flex flex-row-reverse items-center gap-x-1 text-xl',
       },
     },
     defaultVariants: {
@@ -30,16 +37,39 @@ const ButtonVariant = cva(
   },
 );
 
-type ButtonProps = Readonly<
-  HeadlessButtonProps & VariantProps<typeof ButtonVariant>
+type ButtonIcon = Uppercase<
+  NonNullable<VariantProps<typeof ButtonVariant>['icon']>
 >;
 
-const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+const BUTTON_ICONS: Readonly<
+  Record<ButtonIcon, React.FC<React.ComponentProps<'svg'>>>
+> = {
+  'CHEVRON-DOWN': ChevronDownIcon,
+  PLUS: PlusIcon,
+};
+
+type ButtonProps = Readonly<
+  Omit<HeadlessButtonProps, 'children'> &
+    VariantProps<typeof ButtonVariant> &
+    React.PropsWithChildren
+>;
+
+const Button: React.FC<ButtonProps> = ({
+  className,
+  icon,
+  children,
+  ...props
+}) => {
+  const Icon = icon ? BUTTON_ICONS[icon.toUpperCase() as ButtonIcon] : null;
+
   return (
     <HeadlessButton
-      className={cn(ButtonVariant(props), className)}
+      className={cn(ButtonVariant({ icon, ...props }), className)}
       {...props}
-    />
+    >
+      {children}
+      {Icon && <Icon className="size-4.5 stroke-white stroke-2" />}
+    </HeadlessButton>
   );
 };
 
