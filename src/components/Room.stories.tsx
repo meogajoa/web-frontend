78f153c15dd00b9038bf72c70b8863e91a828901
Jehee Cheon <jeheecheon@gmail.com>
2025-01-04 15:11:45 +0900

    Feat-Room component 만듭니다 (#21)
    * feat: add LockIcon svg component
    
    * feat: add Room component

---
+++ src/components/Room.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Room from '~/components/Room';
+
+const meta: Meta<typeof Room> = {
+  title: 'Atoms/Room',
+  component: Room,
+  tags: ['autodocs'],
+  argTypes: {
+    title: {
+      description: 'Room title',
+      control: 'text',
+    },
+    description: {
+      description: 'Room description',
+      control: 'text',
+    },
+    isPrivate: {
+      description: 'Is room private',
+      control: 'boolean',
+    },
+    current: {
+      description: 'Current room number',
+      control: {
+        type: 'range',
+        min: 1,
+        max: 8,
+        step: 1,
+      },
+    },
+    total: {
+      description: 'Total room number',
+      control: {
+        type: 'range',
+        min: 1,
+        max: 8,
+        step: 1,
+      },
+    },
+  },
+  args: {
+    title: 'Room Title',
+    description: 'Room Description',
+    isPrivate: false,
+    current: 1,
+    total: 8,
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Room>;
+
+export const Default: Story = {
+  args: {
+    className: '',
+  },
+};
