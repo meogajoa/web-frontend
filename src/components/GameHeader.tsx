d90096fb7b0e7639792dd9d798c65fda40f8b53a
jeheecheon <jeheecheon@gmail.com>
2025-01-13 00:58:38 +0900

    feat: 게임창에서 시간 추가/감소 버튼 ui 추가

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -1,4 +1,5 @@
 import { Button as HeadlessuiButton } from '@headlessui/react';
+import { MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 import CartFillIcon from '~/svgs/CartFillIcon';
@@ -24,11 +25,11 @@ const GameHeader: React.FC<Props> = ({
   return (
     <header
       className={cn(
-        'flex h-[5.5rem] items-center justify-between drop-shadow-lg',
+        'relative flex h-[5.5rem] items-center justify-between',
         className,
       )}
     >
-      <div className="flex items-center gap-x-2">
+      <div className="z-10 flex items-center gap-x-2">
         <p className="text-2xl font-semibold">
           {`${nthRound}${t('header.nth')} ${isMorning ? t('header.morning') : t('header.night')}`}
         </p>
@@ -38,7 +39,7 @@ const GameHeader: React.FC<Props> = ({
         </p>
       </div>
 
-      <div className="flex items-center gap-x-6">
+      <div className="z-10 flex items-center gap-x-6">
         <span className="text-lg font-bold">₩10</span>
 
         <HeadlessuiButton>
@@ -49,8 +50,45 @@ const GameHeader: React.FC<Props> = ({
           <ChatIcon className="size-6 fill-gray-1" />
         </HeadlessuiButton>
       </div>
+
+      <BottomShadow />
+
+      <div className="absolute bottom-0 left-1/2 z-50 flex -translate-x-1/2 translate-y-1/2 gap-x-1 rounded-full bg-gray-3 p-1">
+        <HeadlessuiButton
+          className="rounded-full bg-gray-3 drop-shadow-lg"
+          onClick={handleMinusClick}
+        >
+          <MinusIcon className="size-6 fill-gray-1" />
+        </HeadlessuiButton>
+
+        <span className="flex h-5.5 w-16 items-center justify-center rounded-full bg-gray-1 text-sm text-white">
+          00:20
+        </span>
+
+        <HeadlessuiButton
+          className={`rounded-full bg-gray-3 drop-shadow-lg`}
+          onClick={handlePlusClick}
+        >
+          <PlusIcon className="size-6 fill-gray-1" />
+        </HeadlessuiButton>
+      </div>
     </header>
   );
+
+  function handleMinusClick() {
+    console.log('minus');
+  }
+
+  function handlePlusClick() {
+    console.log('plus');
+  }
 };
+
+const BottomShadow: React.FC = () => (
+  <div
+    className="pointer-events-none absolute inset-0 size-full bg-inherit drop-shadow-lg"
+    aria-hidden
+  />
+);
 
 export default GameHeader;
