12d506ff3c7d126ae96f2d1f93f2aafda55b0d9f
Jack <pkhjack2325@gmail.com>
2025-01-12 18:15:39 +0900

    FEAT-입력컴포넌트를 생성한다. (#25)
    * feat:입력 컴포넌트와 레이블 컴포넌트를 만들었습니다.
    
    * Refactor:입력 컴포넌트수정-Input스토리북 추가, 코드수정
    
    * refactor: value, onValueChange 추가
    
    ---------
    
    Co-authored-by: jeheecheon <jeheecheon@gmail.com>

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,92 @@
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
+import { cva, VariantProps } from 'class-variance-authority';
+import React from 'react';
+import { cn } from '~/utils/classname';
+
+type InputProps = VariantProps<typeof InputVariant> & {
+  className?: React.ComponentProps<'div'>['className'];
+  value: string;
+  onValueChange: (value: string) => void;
+  type?: 'text' | 'password' | 'email' | 'textarea';
+  placeholder?: string;
+};
+
+const InputVariant = cva(
+  'w-full text-left font-bold focus:border focus:border-red focus:outline-none',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-white font-normal text-gray-1',
+      },
+      rounded: {
+        full: 'rounded-full',
+        lg: 'rounded-lg',
+        md: 'rounded-md',
+      },
+      size: {
+        lg: 'h-11 px-4 py-3.5 text-base',
+        md: 'h-10 px-3 py-2.5 text-sm',
+        sm: 'h-8 px-2 py-1.5 text-sm',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      rounded: 'lg',
+      size: 'md',
+    },
+  },
+);
+
+const Input: React.FC<InputProps> = ({
+  className,
+  value,
+  onValueChange,
+  type = 'text',
+  placeholder,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const inputClassName = cn(InputVariant(props), className);
+  const handlePasswordVisibility = () => setShowPassword((prev) => !prev);
+
+  return (
+    <div className={cn('relative w-full', className)} {...props}>
+      {type === 'textarea' ? (
+        <textarea
+          className={cn(inputClassName, 'resize-none')}
+          placeholder={placeholder}
+        />
+      ) : (
+        <input
+          className={inputClassName}
+          type={type === 'password' && showPassword ? 'text' : type}
+          value={value}
+          placeholder={placeholder}
+          onChange={handleValueChange}
+        />
+      )}
+
+      {type === 'password' && (
+        <button
+          className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+          type="button"
+          aria-label={showPassword ? 'Hide password' : 'Show password'}
+          onClick={handlePasswordVisibility}
+        >
+          {showPassword ? (
+            <EyeIcon className="size-5" />
+          ) : (
+            <EyeSlashIcon className="size-5" />
+          )}
+        </button>
+      )}
+    </div>
+  );
+
+  function handleValueChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onValueChange(event.target.value);
+  }
+};
+
+export default Input;
+export type { InputProps };
