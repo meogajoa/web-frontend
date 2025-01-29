91c2df3472075db65ddb775344164e4c105d9e62
Jack <pkhjack2325@gmail.com>
2025-01-29 16:56:05 +0900

    feat/jackihyun/작은 공지 3개 타입으로 만들었음. (#42)
    * feat/jackihyun/작은 공지 3개 타입으로 만들었음.
    
    * pop을 위해 커밋
    
    * feat/jackihyun/작은 공지 컴포넌트 수정했고, 넘버 아이콘 컴포넌트 만들어서 팀 공지 컴포넌트에 적용해놨습니다.
    
    * feat/커밋 추가(NumberIconBall, 스토리북)
    
    * Feat: 작은 공지 내용 분리된 상태에서 조립가능한 컴포넌트 만듭니다.(#41)
    * feat: GameResultNotice 및 다른 컴포넌트 최적화
    
    * feat: VoteNoticeItem에 profileColor추가하여 받아와서 색 표현.
    
    * test: 스토리북에서 resultType제거 후 argType 추가.
    
    *feat: 각각의 공지들 w 값을 props로 받아서 w값 사용자가 변경 가능.
    
    *refactor: types.ts 새로 만들어 코드 정리
    
    * refactor: GameResultNotice component
    
    ---------
    
    Co-authored-by: jeheecheon <jeheecheon@gmail.com>

---
+++ src/components/GameResultNotice/TeamNoticeItem.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Team } from '~/types/game';
+import { cn } from '~/utils/classname';
+import NumberIconBall from '../NumberIconBall';
+import BaseNoticeItem from './BaseNoticeItem';
+
+export type TeamNoticeItemProps = {
+  rank: number;
+  teamName: string;
+  numberIcons: { number: number; team: Team }[];
+  variant?: 'default' | 'dark';
+  prize: number;
+  className?: string;
+};
+
+const TeamNoticeItem: React.FC<TeamNoticeItemProps> = ({
+  rank,
+  teamName,
+  numberIcons,
+  variant = 'default',
+  prize,
+  className,
+}) => {
+  return (
+    <BaseNoticeItem variant={variant} className={cn('flex', className)}>
+      <div className="flex items-center">
+        <div className="mr-4 pr-2">{rank}등</div>
+        <div className="mr-2 px-2">{teamName}팀</div>
+        <div className="flex gap-1">
+          {numberIcons.map(({ number, team }, index) => (
+            <NumberIconBall key={index} number={number} color={team} />
+          ))}
+        </div>
+      </div>
+
+      <div className="ml-auto text-white">₩{prize}</div>
+    </BaseNoticeItem>
+  );
+};
+
+export default TeamNoticeItem;
