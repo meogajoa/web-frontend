9c11663c396faa82588d741698bbe38db4887f88
jeheecheon <jeheecheon@gmail.com>
2025-01-30 12:07:48 +0900

    feat: split Loading UI into a separate LoadingIndicator component

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { HashLoader } from 'react-spinners';
+import { useDotsString } from '~/hooks/loading';
+import { cn } from '~/utils/classname';
+
+type Props<T> = T & {
+  className?: React.ComponentProps<'div'>['className'];
+  label: React.ReactNode;
+  loaderComponent?: React.ReactNode | React.FC<T>;
+};
+
+const LoadingIndicator = <T extends object = {}>({
+  className,
+  label,
+  loaderComponent: LoaderComponent,
+  ...props
+}: Props<T>) => {
+  const dots = useDotsString({ maxLength: 3 });
+  const styles = getComputedStyle(document.documentElement);
+  const brandRed = styles.getPropertyValue('--color-red');
+
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-center justify-center gap-y-4 font-semibold',
+        className,
+      )}
+    >
+      {!LoaderComponent ? (
+        <HashLoader color={brandRed} {...props} />
+      ) : typeof LoaderComponent === 'function' ? (
+        <LoaderComponent {...(props as T)} />
+      ) : (
+        LoaderComponent
+      )}
+
+      <div className="text-red relative">
+        <span>{label}</span>
+        <span className="absolute">{dots}</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingIndicator;
--- src/hooks/loading.ts
+++ src/hooks/loading.ts
@@ -1,21 +1,10 @@
-import { debounce } from 'lodash-es';
 import React from 'react';
-import { A_SECOND } from '~/utils/constants';
-
+import { useInterval } from 'react-use';
 export const useDotsString = ({ maxLength = 3 }: { maxLength?: number }) => {
-  const [dots, setDots] = React.useState('');
+  const [dotsCount, setDotsCount] = React.useState(0);
+  useInterval(() => {
+    setDotsCount((prev) => prev + 1);
+  }, 500);
 
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setDots((dots) => (dots.length >= maxLength ? '' : dots + '.'));
-    }, 500);
-
-    debounce(() => {
-      clearInterval(interval);
-    }, 30 * A_SECOND)();
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return dots;
+  return '.'.repeat(dotsCount % (maxLength + 1));
 };
