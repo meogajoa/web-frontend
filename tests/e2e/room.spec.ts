909783311ecd2463eb435ebb5b53448fa84e1698
jeheecheon <jeheecheon@gmail.com>
2025-02-11 04:04:54 +0900

    test: add room join test

--- tests/e2e/room.spec.ts
+++ tests/e2e/room.spec.ts
@@ -4,6 +4,7 @@ import {
   signInAsync,
   signUpAsync,
 } from '@tests/e2e/utils/auth';
+import { range } from 'lodash-es';
 import { createRoomAsync, generateRandomRoom } from './utils/room';
 
 test.describe('Create room', () => {
@@ -28,5 +29,25 @@ test.describe('Create room', () => {
       room,
       shouldSuccess: true,
     });
+  });
+});
+
+test.describe('Join room', () => {
+  test('should join room with 7 accounts', async ({ page }) => {
+    const accounts = range(7).map(() => generateRandomAccount());
+
+    for (const account of accounts) {
+      await signUpAsync(page, {
+        account,
+        shouldSuccess: true,
+      });
+
+      await signInAsync(page, {
+        account,
+        shouldSuccess: true,
+      });
+
+      await page.getByRole('link', { name: 'test1' }).click();
+    }
   });
 });
